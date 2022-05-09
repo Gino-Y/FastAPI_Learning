0e26e03d800e2e926dd3b9cb9c96f4f7ddf4de4b
Gino-Y <ginoyan250@gmail.com>
2022-05-09 22:33:39 +0800

    2

--- chapter01/test03.py
+++ chapter01/test03.py
@@ -1,7 +1,28 @@
+"""
+typing
+"""
+from typing import List, Union, NewType
 
-name = 'Gino'
-name.split('.')
+
+def get_name(name: str) -> str:
+    new_name = name.split('.')[0]
+    return new_name
 
 
-def get_name(user: str):
-    user.split()
+def test_sum(num: int) -> int:
+    return num + 1
+
+
+def is_ok(b: bool) -> str:
+    if b:
+        return 'ok'
+    else:
+        return 'not ok'
+
+
+my_list = List[int or str]
+
+
+def get_new_list(li: my_list) -> Union[int, str, bool]:
+    return li
+
