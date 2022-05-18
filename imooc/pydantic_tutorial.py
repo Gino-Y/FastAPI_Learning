a0576144134bec76bfb0cc148dfa6f6b94808ab0
Gino-Y <ginoyan250@gmail.com>
2022-05-18 15:11:23 +0800

    moke

--- imooc/pydantic_tutorial.py
+++ imooc/pydantic_tutorial.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from pydantic import BaseModel
+from pydantic import BaseModel, ValidationError
 from typing import List, Optional
 
 
@@ -23,3 +23,16 @@ user = User(**external_data)
 print(user.id, user.friends)
 print(repr(user.signup_ts))
 print(user.dict())
+
+print('\033[21m --- 校验失败处理 ---\033[0m')
+try:
+    User(id=1,
+         signup_ts=datetime.today(),
+         friends=[1, 2, 'not number'])
+except ValidationError as e:
+    print(e.json())
+
+print('------------------')
+print(user.dict())
+print(user.json())
+print(user.copy())
