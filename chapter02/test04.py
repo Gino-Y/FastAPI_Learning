8290534d8f911daff9a3450b516d1e796e553672
Gino-Y <ginoyan250@gmail.com>
2022-05-26 14:17:09 +0800

    文件上传

--- chapter02/test04.py
+++ chapter02/test04.py
@@ -1,31 +1,43 @@
-from fastapi import FastAPI, Response
+from fastapi import FastAPI, Response, status
+from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 from typing import Union
 
 app = FastAPI(
-    title='Response Test',
-    description='返回数据的集中方式'
+    title='Response API',
+    description='返回数据的几种方式'
 )
 
 
-@app.get('/ret_str', tags=['返回字符串'])
+@app.get('/ret_str',
+         tags=['返回字符串'],
+         summary='将得到一个字符串',
+         response_description='应答一行字符串',
+         status_code=status.HTTP_200_OK)  # 状态码设置
 def ret_str():
     return 'hello fastapi'
 
 
-@app.get('/ret_dict', tags=['返回字典'])
+@app.get('/ret_dict',
+         tags=['返回字典'],
+         summary='将得到一个字典')
 def ret_dict():
     return {'id': 1, 'mane': 'Gino'}
 
 
-@app.get('/ret_json', tags=['返回Json'])
+@app.get('/ret_json',
+         tags=['返回Json'],
+         summary='用JSONResponse, 字典转为json')
 def ret_json():
     content = {'id': 1, 'mane': 'Gino'}
     return JSONResponse(content=content)
 
 
-@app.get('/ret_json2', tags=['返回Json'])
+@app.get('/ret_json2',
+         tags=['返回Json'],
+         summary='字符串转为json(并不成功)',
+         deprecated=True)  # 弃用
 def ret_json():
     """
     这种也不行啊
@@ -35,13 +47,17 @@ def ret_json():
     return JSONResponse(content=content)
 
 
-@app.get('/ret_header', tags=['返回到header'])
+@app.get('/ret_header',
+         tags=['返回到header'],
+         summary='将得到一条headers记录')
 def ret_header(rsp: Response):
     rsp.headers['name'] = 'Gino'
     return 'hello fastapi'
 
 
-@app.get('/ret_header2', tags=['返回到header'])
+@app.get('/ret_header2',
+         tags=['返回到header', '返回Json'],
+         summary='将得到一条JSON数据,和两条headers记录')
 def ret_header():
     content = {'age': 18}
     headers = {'name': 'Gino', 'addr': 'HangZhou'}
@@ -76,14 +92,20 @@ def ret_model_data():
     return user
 
 
-# @app.get('/ret_model3', tags=['返回模型'], response_model=Union[RetUser, Article])
+# @app.get('/ret_model3', tags=['返回模型'], response_model=RetUser)
 # def ret_model_data():
 #     user = User(id=1, name='Gino', pwd='123456')
 #     article = Article(id=2, title='Article Test')
-#     return user
+#     return jsonable_encoder('user', article)
 
 
-
+@app.get('/ret_jsonable',
+         tags=['返回Json'],
+         summary='用jsonable_encoder, 字典转为json')
+def ret_jsonable():
+    content = {'id': 2}
+    json_content = jsonable_encoder(content)
+    return json_content
 
 
 
---
+++ chapter02/test05.py
@@ -0,0 +1,40 @@
+from fastapi import (FastAPI,
+                     UploadFile,
+                     File)
+
+app = FastAPI(
+    title='upload API',
+    description='单文件、多文件上传的接口'
+)
+
+
+# @app.post('/upload',
+#           tags=['单文件上传'],
+#           summary='上传一个文件',
+#           response_description='上传了一个文件',
+#           )
+# async def upload(file: UploadFile = File(...)):
+#     print(file.file)
+#     rep = await file.read()
+#     with open("../upload/chapter02/" + file.filename, 'wb') as f:
+#         f.write(rep)
+#     return '上传成功'
+
+@app.post("/upload")
+async def upload(file:UploadFile= File(...)):
+    print(file.file)
+    rep = await file.read()
+    with open("../upload/chapter02/"+file.filename,"wb") as f:
+        f.write(rep)
+    return "上传成功"
+
+
+
+
+
+
+
+
+
+
+
