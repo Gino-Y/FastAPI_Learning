aca742fb4a4ca6c43cdb341de35150c0da5c5a54
Gino-Y <ginoyan250@gmail.com>
2022-06-03 22:31:54 +0800

    1

--- chapter02/test08_2.py
+++ chapter02/test08_2.py
@@ -3,6 +3,9 @@ from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from chapter02 import test08_utils
 from datetime import timedelta
 
+EXPIRE_MINUTE = 60
+
+
 app = FastAPI(
     title='attestation Test',
     description='身份认证'
@@ -17,9 +20,12 @@ def login(user: OAuth2PasswordBearer = Depends()):
     hash_pwd = test08_utils.get_hash_pwd(password)
     # todo用户名密码校验
     is_ok = True
+
+    expire_time = timedelta(minutes=EXPIRE_MINUTE)
     if is_ok:
-        test08_utils.create_token({'useername': user})
-        return {'code': 200, 'msg': '登陆成功', 'token': 'xxxxxxx'}
+        # 生成token
+        token = test08_utils.create_token({'sub': usermane}, expire_time)
+        return {'code': 200, 'msg': '登陆成功', 'token': token}
     else:
         return {'code': 401, 'msg': '认证不通过', 'token': ''}
 
--- chapter02/test08_utils.py
+++ chapter02/test08_utils.py
@@ -1,10 +1,12 @@
 from passlib.context import CryptContext
 from jose import jwt
+from datetime import datetime
 
 crypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 SECRET_KEY = 'lasdfjlsdfjlasdkjflsadkjflasdjflasdkjkflasdfjlsakdfjalsdjflsdfjlsdkfjlskdjflskdfj'
 
 ALGORITHM = 'HS256'
+
 
 # 明文密码加密
 def get_hash_pwd(pwd: str):
@@ -13,6 +15,8 @@ def get_hash_pwd(pwd: str):
 
 # 生成token: 用户数据，token过期时间
 def create_token(data: dict, expire_time):
+
+    data.update({'exp': expire_time})
     token = jwt.encode(data, SECRET_KEY, ALGORITHM)
     return token
 
